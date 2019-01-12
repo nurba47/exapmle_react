58caa685260f6df86fee3befdbfc663eded8773b
nurba <nurba47@gmail.com>
2019-01-12 12:34:00 +0600

    Update

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -24,13 +24,13 @@ const TodoListItem = ({ important, done,
       <button type="button"
               className="btn btn-outline-success btn-sm float-right"
               onClick={onToggleImportant}>
-        <i className="fa fa-exclamation"></i>
+        <i className="fa fa-exclamation"> </i>
       </button>
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right"
               onClick={onDelete}>
-        <i className="fa fa-trash-o"></i>
+        <i className="fa fa-trash-o"> </i>
       </button>
     </span>
   );
